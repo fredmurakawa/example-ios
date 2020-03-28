9a6e53e415116c9d028145bede29d84ec77b40f6
Fred Murakawa <fredbmurakawa@gmail.com>
2020-03-28 17:14:00 -0300

    Setup UISplitViewController in SceneDelegate

--- frederico-murakawa-ios/ViewController.swift
+++ frederico-murakawa-ios/ViewController.swift
@@ -12,9 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .red
+        navigationItem.leftItemsSupplementBackButton = true
+        navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+
+        let imageView = UIImageView(frame: CGRect(x: 300, y: 100, width: 100, height: 100))
+        view.addSubview(imageView)
+
+        imageView.loadImage(at: URL(string: "http://res.cloudinary.com/cheesecakelabs/image/upload/v1488993901/challenge/news_02_ulyqvw.jpg")!)
+
+        let articleProvider = ArticlesProvider(apiSession: APISession())
+        articleProvider.getArticles { (result) in
+
+            do {
+                let articles = try result.get()
+                print(articles)
+            } catch {
+                print(error)
+            }
+        }
     }
-
-
 }
 
+extension ViewController: NewsFeedVCDelegate {
+    func didSelectArticle(_ article: Article) {
+        print(article)
+    }
+}
