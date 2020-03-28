130442e7735ac141135319b7ba9cf17d8cb5ca94
Fred Murakawa <fredbmurakawa@gmail.com>
2020-03-28 20:01:15 -0300

    Create NewsFeedViewModel

---
+++ frederico-murakawa-ios/ViewModels/NewsFeedViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  NewsFeedViewModel.swift
+//  frederico-murakawa-ios
+//
+//  Created by Frederico Murakawa on 3/28/20.
+//  Copyright © 2020 Frederico Murakawa. All rights reserved.
+//
+
+import Foundation
+
+public class NewsFeedViewModel {
+    private let articlesProvider: ArticlesProviding
+    private var articles: [Article] = []
+
+    var onArticlesLoaded: () -> Void = {}
+
+    var numberOfRowsInSection: Int { articles.count }
+
+    init(articlesProvider: ArticlesProviding) {
+        self.articlesProvider = articlesProvider
+    }
+
+    func loadArticles() {
+        articlesProvider.getArticles { [weak self] result in
+            do {
+                self?.articles = try result.get()
+                self?.onArticlesLoaded()
+            } catch {
+                #warning("Implement")
+            }
+        }
+    }
+}
