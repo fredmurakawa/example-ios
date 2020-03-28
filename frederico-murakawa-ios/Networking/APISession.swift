a887960c041e0f2c223eee8a7ad07a69863d4d21
Fred Murakawa <fredbmurakawa@gmail.com>
2020-03-27 21:16:27 -0300

    Create APISession

---
+++ frederico-murakawa-ios/Networking/APISession.swift
@@ -0,0 +1,44 @@
+//
+//  APISession.swift
+//  frederico-murakawa-ios
+//
+//  Created by Frederico Murakawa on 3/27/20.
+//  Copyright © 2020 Frederico Murakawa. All rights reserved.
+//
+
+import Foundation
+
+protocol RequestProviding {
+    var urlRequest: URLRequest { get }
+}
+
+protocol APISessionProviding {
+    func fetch<T: Decodable>(_ requestProvider: RequestProviding, completion: @escaping (Result<T, Error>) -> Void)
+}
+
+public struct APISession: APISessionProviding {
+    init() {}
+
+    func fetch<T: Decodable>(_ requestProvider: RequestProviding, completion: @escaping (Result<T, Error>) -> Void) {
+        let urlRequest = requestProvider.urlRequest
+
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            do {
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+
+                guard let data = data else {
+                    preconditionFailure("No error was received but we also don't have data.")
+                }
+
+                let decodedObject = try JSONDecoder().decode(T.self, from: data)
+
+                completion(.success(decodedObject))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
