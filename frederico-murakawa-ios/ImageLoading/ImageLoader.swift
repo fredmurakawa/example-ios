64df06af679e6d88455bc2139eca5a4a1f19eac8
Fred Murakawa <fredbmurakawa@gmail.com>
2020-03-27 23:57:46 -0300

    Create ImageLoader and ImageViewLoader

---
+++ frederico-murakawa-ios/ImageLoading/ImageLoader.swift
@@ -0,0 +1,75 @@
+//
+//  ImageLoading.swift
+//  frederico-murakawa-ios
+//
+//  Created by Frederico Murakawa on 3/27/20.
+//  Copyright © 2020 Frederico Murakawa. All rights reserved.
+//
+
+import UIKit
+
+internal class ImageLoader {
+    private var loadedImages = [URL: UIImage]() // In-memory cache
+    private var runningRequests = [UUID: URLSessionDataTask]() // Keep a reference to data task to cancel it later if needed
+    private var dispatchQueue = DispatchQueue(label: "com.fred.ImageLoader")
+
+    func loadImage(_ url: URL, _ completion: @escaping (Result<UIImage, Error>) -> Void) -> UUID? {
+
+        if let image = loadedImageFor(url) {
+            completion(.success(image))
+            return nil
+        }
+
+        let uuid = UUID()
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            defer { self.runningRequests.removeValue(forKey: uuid) }
+
+            if let data = data, let image = UIImage(data: data) {
+                self.setLoadedImage(image, for: url)
+                completion(.success(image))
+                return
+            }
+
+            guard let error = error else {
+                // without an image or an error, we'll just ignore this for now
+                #warning("Implement this case")
+                return
+            }
+
+            // Check if the request was cancelled
+            guard (error as NSError).code == NSURLErrorCancelled else {
+                completion(.failure(error))
+                return
+            }
+
+            // the request was cancelled, no need to call the callback
+        }
+        task.resume()
+
+        dispatchQueue.sync {
+            runningRequests[uuid] = task
+        }
+
+        return uuid
+    }
+
+    func cancelLoad(_ uuid: UUID) {
+        dispatchQueue.sync {
+            runningRequests[uuid]?.cancel()
+            runningRequests.removeValue(forKey: uuid)
+        }
+    }
+
+    private func loadedImageFor(_ url: URL) -> UIImage? {
+        return dispatchQueue.sync {
+            return self.loadedImages[url]
+        }
+    }
+
+    private func setLoadedImage(_ image: UIImage, for url: URL) {
+        dispatchQueue.sync {
+            self.loadedImages[url] = image
+        }
+    }
+}
---
+++ frederico-murakawa-ios/ImageLoading/ImageViewLoader.swift
@@ -0,0 +1,56 @@
+//
+//  ImageViewLoader.swift
+//  frederico-murakawa-ios
+//
+//  Created by Frederico Murakawa on 3/27/20.
+//  Copyright © 2020 Frederico Murakawa. All rights reserved.
+//
+
+import UIKit
+
+internal class ImageViewLoader {
+    static let loader = ImageViewLoader()
+
+    private let imageLoader = ImageLoader()
+    private var uuidMap = [UIImageView: UUID]() // Tie Image View with UUID to cancel loading tasks later if needed
+
+    private init() {}
+
+    fileprivate func load(_ url: URL, for imageView: UIImageView) {
+        let uuid = imageLoader.loadImage(url) { result in
+            defer { self.uuidMap.removeValue(forKey: imageView) }
+
+            do {
+                let image = try result.get()
+                DispatchQueue.main.async {
+                    imageView.image = image
+                }
+            } catch {
+                print("Failed to fetch image at: \(url.absoluteString)", error)
+                #warning("Add a placeholder image?")
+            }
+        }
+
+        if let uuid = uuid {
+            uuidMap[imageView] = uuid
+        }
+    }
+
+    fileprivate func cancel(for imageView: UIImageView) {
+        if let uuid = uuidMap[imageView] {
+            imageLoader.cancelLoad(uuid)
+            uuidMap.removeValue(forKey: imageView)
+        }
+    }
+}
+
+extension UIImageView {
+    public func loadImage(at url: URL) {
+        ImageViewLoader.loader.load(url, for: self)
+    }
+
+    public func cancelImageLoad() {
+        ImageViewLoader.loader.cancel(for: self)
+    }
+}
+
