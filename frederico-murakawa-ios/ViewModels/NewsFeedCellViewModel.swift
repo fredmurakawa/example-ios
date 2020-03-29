10977a1968ca8ad746d697b1afabb1a037147672
Fred Murakawa <fredbmurakawa@gmail.com>
2020-03-28 21:15:48 -0300

    Create NewsFeedCellViewModel

---
+++ frederico-murakawa-ios/ViewModels/NewsFeedCellViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  NewsFeedCellViewModel.swift
+//  frederico-murakawa-ios
+//
+//  Created by Frederico Murakawa on 3/28/20.
+//  Copyright © 2020 Frederico Murakawa. All rights reserved.
+//
+
+import Foundation
+
+public class NewsFeedCellViewModel {
+    private let article: Article
+
+    var onArticlesLoaded: () -> Void = {}
+
+    init(article: Article) {
+        self.article = article
+    }
+
+    var title: String { article.title }
+    var authors: String { article.authors }
+    var date: String { article.date }
+}
