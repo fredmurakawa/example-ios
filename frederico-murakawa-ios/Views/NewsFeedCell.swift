a50e3bbd0c198d76a2ed03f9c552706afd5cfb79
Fred Murakawa <fredbmurakawa@gmail.com>
2020-03-28 21:45:03 -0300

    Fix outlets in NewsFeedCell and make them private

--- frederico-murakawa-ios/Views/NewsFeedCell.swift
+++ frederico-murakawa-ios/Views/NewsFeedCell.swift
@@ -11,21 +11,30 @@ import UIKit
 class NewsFeedCell: UITableViewCell {
     static let reuseIdentifier = "NewsFeedCell"
 
-    @IBOutlet weak var articleImageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var authorsLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet private weak var articleImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var authorsLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
 
-        articleImageView.layer.cornerRadius = articleImageView.bounds.width / 2
+        articleImageView?.layer.cornerRadius = articleImageView.bounds.width / 2
     }
 
     override func prepareForReuse() {
         super.prepareForReuse()
 
-        articleImageView.cancelImageLoad()
-        articleImageView.image = nil
+        articleImageView?.cancelImageLoad()
+        articleImageView?.image = nil
+    }
+
+    func configure(with viewModel: NewsFeedCellViewModel) {
+        titleLabel?.text = viewModel.title
+        authorsLabel?.text = viewModel.authors
+        dateLabel?.text = viewModel.date
+        if let url = URL(string: viewModel.imageURL) {
+            articleImageView?.loadImage(at: url)
+        }
     }
 }
